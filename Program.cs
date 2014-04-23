90a334862e3742f1b3247c412cd2e637ad14f565
Akhier <akhierthedragonhearted@yahoo.com>
2014-04-23 14:02:40 -0400

    *Window Size now a Const

--- Program.cs
+++ Program.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 using libtcod;
 using CSharpHelperCode;
 using CSharpSimpleMapGen;
-using CSharpDijkstraAlgorithm; //the tiles are 8x8
+using CSharpDijkstraAlgorithm;
 namespace CSharpShowDijkstraWorking {
     class Program {
+        const int windowWidth = 100, windowLength = 100;
         static void Main(string[] args) {
-            TCODConsole.initRoot(80, 50, "Showing Dijkstra Algorithm Working");
+            TCODConsole.initRoot(windowWidth, windowLength, "Showing Dijkstra Algorithm Working");
             TCODSystem.setFps(30);
             Graph map = makeMap();
             TCODMouseData mData = TCODMouse.getStatus();
@@ -38,18 +39,18 @@ namespace CSharpShowDijkstraWorking {
         }
         static Graph makeMap() {
             Graph output = new Graph();
-            bool[,] boolMap = MapGen.newMap(80, 50, true);
-            Vector2D[,] vectorMap = new Vector2D[80, 50];
-            for (int column = 0; column < 50; column++) {
-                for (int row = 0; row < 80; row++) {
+            bool[,] boolMap = MapGen.newMap(windowWidth, windowLength, true);
+            Vector2D[,] vectorMap = new Vector2D[windowWidth, windowLength];
+            for (int column = 0; column < windowLength; column++) {
+                for (int row = 0; row < windowWidth; row++) {
                     if (boolMap[row, column]) {
                         vectorMap[row, column] = new Vector2D(row, column, false);
                         output.addVector(vectorMap[row, column]);
                     }
                 }
             }
-            for (int column = 1; column < 49; column++) {
-                for (int row = 1; row < 79; row++) {
+            for (int column = 1; column < windowLength-1; column++) {
+                for (int row = 1; row < windowWidth-1; row++) {
                     if (boolMap[row, column]) {
                         if (boolMap[row - 1, column - 1]) { output.addEdge(new Edge(vectorMap[row, column], vectorMap[row - 1, column - 1], 11)); }
                         if (boolMap[row - 1, column]) { output.addEdge(new Edge(vectorMap[row, column], vectorMap[row - 1, column], 10)); }
@@ -78,8 +79,8 @@ namespace CSharpShowDijkstraWorking {
             TCODConsole.root.setBackgroundColor(new TCODColor(15, 15, 15));
             TCODConsole.root.setForegroundColor(TCODColor.lightestGrey);
             TCODConsole.root.clear();
-            for (int column = 0; column < 50; column++) {
-                for (int row = 0; row < 80; row++) {
+            for (int column = 0; column < windowLength; column++) {
+                for (int row = 0; row < windowWidth; row++) {
                     TCODConsole.root.putChar(row, column, '#');
                 }
             }
