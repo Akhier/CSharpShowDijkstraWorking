3f95d85043150eb743750e9ab987da9885aeff44
akhier <akhierthedragonhearted@yahoo.com>
2014-04-22 21:55:52 -0400

    Basic Setup of libraries and Libtcod

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//By: Akhier Dragonheart
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libtcod;
+using CSharpHelperCode;
+using CSharpSimpleMapGen;
+namespace CSharpShowDijkstraWorking {
+    class Program {
+        static void Main(string[] args) {
+            TCODConsole.initRoot(80, 50, "Showing Dijkstra Algorithm Working");
+            TCODConsole.waitForKeypress(true);
+        }
+    }
+}
