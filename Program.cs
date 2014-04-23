7dea16a2d95f8c269868335250d855ef772e0793
akhier <akhierthedragonhearted@yahoo.com>
2014-04-22 22:07:58 -0400

    +Basic Setup

--- Program.cs
+++ Program.cs
@@ -7,11 +7,24 @@ using System.Threading.Tasks;
 using libtcod;
 using CSharpHelperCode;
 using CSharpSimpleMapGen;
+using CSharpDijkstraAlgorithm;
 namespace CSharpShowDijkstraWorking {
     class Program {
         static void Main(string[] args) {
             TCODConsole.initRoot(80, 50, "Showing Dijkstra Algorithm Working");
-            TCODConsole.waitForKeypress(true);
+            Graph map = new Graph();
+            TCODConsole.waitForKeypress(true);            
+        }
+
+        static Graph makeMap() {
+            Graph output = new Graph();
+            bool[,] boolMap = MapGen.newMap(80, 50, true);
+            for (int column = 0; column < 50; column++) {
+                for (int row = 0; row < 80; row++) {
+                    
+                }
+            }
+            return output;
         }
     }
 }
